5a33fc7766b6411d62ee21dd49e39a62e702f9c2
ahmed samir sakr <89707420+ahmedsamir010@users.noreply.github.com>
2022-01-19 20:32:29 +0200

    Add files via upload

---
+++ Ultra-Fast Mathematician.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include<iomanip>
+#include<cmath>
+#include<string>
+#include<algorithm>
+#define ahmed  ios_base::sync_with_stdio(false);cout.tie(NULL);cin.tie(NULL);
+using namespace std;
+#define ll long long
+#define ld long double
+int counter[1000001] = {};
+long long arr[1000000] = {};
+int main()
+{
+    ahmed;
+    string s1, s2,s3;
+    cin >> s1 >> s2;
+    for (size_t i = 0; i < s1.length(); i++)
+    {
+        if (s1[i] == s2[i])
+            s3+='0';
+        else s3 +='1';
+    }cout << s3 << endl;
+   
+    return 0;
+}
+//مهما الدنيا إتنيلت ضلمت لازم يبقى عندك زفت أمل و تعرف إن في نور في أخر أم النفق//
+
+        //اهو بنتنيل نستمر ف السعي رغم فقدان الزفت الشغف
